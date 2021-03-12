92c0ac7a079fcd5a5476f25c3e3f09521b55c15b
handsupmin <72061607+handsupmin@users.noreply.github.com>
2021-03-12 11:59:53 +0900

    InterForm Control Application

---
+++ SecondFormApplication/SecondFormApplication/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SecondFormApplication
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // 두 번째 폼을 메뉴를 이용하여 생성
+        private void 두번째폼열기ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Form2 form = new Form2(); // Form2 생성
+            form.Show(); // 화면에 띄워줌
+        }
+
+        // 폼 제거를 누르면 호출되는 이벤트 함수
+        private void 폼제거ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (Application.OpenForms["CustomForm"] != null) // CustomForm이 열려있다면
+            {
+                Application.OpenForms["CustomForm"].Close(); // CustomForm을 먼저 닫음
+                return; // 함수 종료
+            } else if(Application.OpenForms["Form2"] != null) // Form2가 열려있다면
+            {
+                Application.OpenForms["Form2"].Close(); // Form2를 닫음
+            } // Form2나 CustomForm이 없다면 아무일도 일어나지 않음
+        }
+    }
+}
---
+++ SecondFormApplication/SecondFormApplication/Form2.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SecondFormApplication
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        public class CustomForm : Form // Form 클래스를 상속받는 CustomForm 클래스
+        {
+            public CustomForm() // 생성자 작성
+            {
+                Name = "CustomForm"; // 이름을 CustomForm,
+                Text = "세 번째 폼"; // 제목을 세 번째 폼 으로 설정
+                this.Size = new Size(524, 451); // 폼 크기 설정
+
+                Button button = new Button(); // 버튼 생성 후
+                Controls.Add(button);
+                button.Location = new Point(206, 191); // 위치 지정
+                button.Text = "세 번째 버튼"; // 버튼의 텍스트 수정
+                button.Width = 100;
+
+                button.Click += new EventHandler(button_Click); // 새로만든 버튼에도 생성 이벤트를 추가
+            }
+
+            public void button_Click(object sender, EventArgs e)
+            {
+                MessageBox.Show("더이상 폼을 만들 수 없습니다.", "알림"); // 버튼을 누르면 메세지 박스 생성
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e) // "두 번째 버튼" 클릭 시
+        {
+            CustomForm custom = new CustomForm(); // CustomForm 생성
+            custom.Show();
+        }
+
+    }
+}
