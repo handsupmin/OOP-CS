d37e9f6d3d3d64dedddbf50205f614c8d072fe24
handsupmin <72061607+handsupmin@users.noreply.github.com>
2021-05-04 19:23:58 +0900

    OOP programming codes

---
+++ template method/template method/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace template_method
+{
+    class Program
+    {
+        interface IBorder
+        {
+            void writeBorder(string content);
+        }
+
+        class Monitor
+        {
+            private IBorder border;
+            public Monitor(IBorder border)
+            {
+                this.border = border;
+            }
+            public void start()
+            {
+                Console.Write("텍스트를 입력해주세요.: ");
+                string context = Console.ReadLine();
+                border.writeBorder(context);
+            }
+        }
+
+        class Star: IBorder
+        {
+            public void writeBorder(string content)
+            {
+                for (int i = 0; i < 14; i++)
+                    Console.Write("*");
+                Console.WriteLine("\n*{0}*",content);
+                for (int i = 0; i < 14; i++)
+                    Console.Write("*");
+                Console.Write("\n");
+            }
+        }
+        class Slash : IBorder
+        {
+            public void writeBorder(string content)
+            {
+                for (int i = 0; i < 14; i++)
+                    Console.Write("/");
+                Console.WriteLine("\n/{0}/", content);
+                for (int i = 0; i < 14; i++)
+                    Console.Write("/");
+                Console.Write("\n");
+            }
+        }
+        class Wave : IBorder
+        {
+            public void writeBorder(string content)
+            {
+                for (int i = 0; i < 14; i++)
+                    Console.Write("~");
+                Console.WriteLine("\n~{0}~", content);
+                for (int i = 0; i < 14; i++)
+                    Console.Write("~");
+                Console.Write("\n");
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Monitor monitor = new Monitor(new Star());
+            monitor.start();
+            monitor = new Monitor(new Slash());
+            monitor.start();
+            monitor = new Monitor(new Wave());
+            monitor.start();
+        }
+    }
+}
---
+++ zookeeper/zookeeper/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zookeeper
+{
+    class Mammal
+    {
+        public void Nurse() { }
+    }
+
+    class Dog : Mammal
+    {
+        public void Bark() { }
+    }
+    class Cat : Mammal
+    {
+        public void Meow() { }
+    }
+    class Elephant : Mammal
+    {
+        public void Pou() { }
+    }
+    class Lion : Mammal
+    {
+        public void Crow() { }
+    }
+
+    class Zookeeper
+    {
+        public void Wash(Mammal mammal) { Console.WriteLine("{0} 동물을 wash합니다.", mammal); }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dog dog = new Dog();
+            Cat cat = new Cat();
+            Elephant elephant = new Elephant();
+            Lion lion = new Lion();
+
+            Zookeeper zookeeper = new Zookeeper();
+            zookeeper.Wash(dog);
+            zookeeper.Wash(cat);
+            zookeeper.Wash(elephant);
+            zookeeper.Wash(lion);
+        }
+    }
+}
