3bce1f6b4643f5c4e2df64b471e03843e82748e7
handsupmin <72061607+handsupmin@users.noreply.github.com>
2021-05-11 21:14:29 +0900

    study property

---
+++ property/property/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace property
+{
+    class NameCard
+    {
+        public string Name
+        {
+            get;
+            set;
+        }
+
+        public int Age
+        {
+            get;
+            set;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            NameCard Mycard = new NameCard()
+            {
+                Name = "상현",
+                Age = 24
+            };
+
+            Console.WriteLine("이름: {0}", Mycard.Name);
+            Console.WriteLine("나이: {0}", Mycard.Age);
+        }
+    }
+}
