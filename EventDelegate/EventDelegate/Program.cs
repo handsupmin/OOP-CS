74ef5f70e569c0c9550ebe539acf1953aa073ffd
handsupmin <72061607+handsupmin@users.noreply.github.com>
2021-05-27 23:48:46 +0900

    델리게이트를 이용하여 이벤트 구현

---
+++ EventDelegate/EventDelegate/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventDelegate
+{
+    delegate void MyDelegate(int a);
+
+    class Market
+    {
+        public event MyDelegate CustomerEvent;
+
+        public void BuySomething(int CustomerNo)
+        {
+            if (CustomerNo == 30)
+                CustomerEvent(CustomerNo);
+        }
+    }
+    
+    class Program
+    {
+        static public void MyHandler(int num)
+        {
+            Console.WriteLine("축하합니다! {0}번째 고객 이벤트에 당첨되셨습니다.", num);
+        }
+
+        static void Main(string[] args)
+        {
+            
+            Market market = new Market();
+            market.CustomerEvent += new MyDelegate(MyHandler);
+
+            for (int customerNo = 0; customerNo < 100; customerNo += 10)
+                market.BuySomething(customerNo);
+        }
+    }
+}
