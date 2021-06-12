374a862ba3f19b265a755c1991a2539e19d78426
handsupmin <72061607+handsupmin@users.noreply.github.com>
2021-06-12 23:54:51 +0900

    nullable을 사용하도록 수정

--- Lambda/Lambda/Program.cs
+++ Lambda/Lambda/Program.cs
@@ -8,54 +8,38 @@ namespace Lambda
 {
     class Program
     {
-        delegate int MyDivide(int a, int b);
+        delegate int? MyDivide(int a, int b); // nullable 사용(int?)
 
         static void Main(string[] args)
         {
             // Anonymous function 사용
-            MyDivide myFunc;
-            myFunc = delegate (int a, int b)
+            MyDivide myFunc = delegate (int a, int b)
             {
                 if (b == 0)
                 {
-                    return -1; // 발견한 버그, null 값은 반환될 수 없음
+                    return null;
                 }
 
                 return a / b;
             };
             void write(int a, int b)
             {
-                int result = myFunc(a, b);
-
-                if (result != -1)
-                    Console.WriteLine("{0} / {1} == {2}", a, b, result);
-                else
-                    Console.WriteLine("{0} / {1} == ", a, b);
+                object result = myFunc(a, b);
+                Console.WriteLine("{0} / {1} == {2}", a, b, result);
             }
 
-            int t1, t2;
-
-            t1 = 10;
-            t2 = 2;
+            int t1 = 10, t2 = 2;
             write(t1, t2);
-
             t2 = 0;
             write(t1, t2);
 
             // Lambda식을 사용하여 구현
-            MyDivide myFunc2;
-
-            myFunc2 = (int a, int b) => { return (a / b); };
-
-            t1 = 10;
+            MyDivide myFunc2 = (int a, int b) => { return (a / b); };
             t2 = 2;
             Console.WriteLine("{0} / {1} == {2}", t1, t2, myFunc2(t1, t2));
 
             // Function Delegate 사용
             Func<int, int, int> myFunc3 = (int a, int b) => { return (a / b); };
-
-            t1 = 10;
-            t2 = 2;
             Console.WriteLine("{0} / {1} == {2}", t1, t2, myFunc3(t1, t2));
 
             // Action Delegate 사용
